b387e17cefac19ee728d2857cf076d0cacfd3a7d
BlueCloud15696 <webmaster15696@gmail.com>
2022-04-19 18:12:15 -0400

    editted: hero text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -345,7 +345,7 @@ function Home({ setAuthState, authState, logout }) {
                   <ScrollingText style={{ display: "inline-block" }} />
                 </span>
                 <br />
-                developers to your team
+                developers editted to your team
               </h1>
               <p>
                 Bolo helps tech companies extend their engineering teams by
